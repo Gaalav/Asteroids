263236ff8783df962109572d4af23bbf493cd50c
Gaalav <gaalavg@gmail.com>
2025-12-18 13:17:37 +0530

    added Player

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from pydoc import plainpager
+
 import pygame
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 
 def main():
@@ -12,13 +15,20 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while 1:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
+        updatable.update(dt)
         dt = clock.tick(60) / 1000
 
 
